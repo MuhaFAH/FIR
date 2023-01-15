d4754399d834e48e5d6d0e394deb89bfff692481
LysakVladimir <vladimirlysak12@gmail.com>
2023-01-15 18:55:44 +0300

    Добавлен игрок и его анимация.

---
+++ player.py
@@ -0,0 +1,100 @@
+import pygame
+
+from enemy import ENEMY_SPRITES
+from images import PLAYER_RECT_IMAGE
+
+PLAYER_SPRITE = pygame.sprite.GroupSingle()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, armor, position):
+        super().__init__(PLAYER_SPRITE)
+
+        self.add(PLAYER_SPRITE)
+        self.image = armor[0][0][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+
+        self.hit_points = 100
+        self.armor_points = 25
+        self.damage = 5
+        self.speed = 3
+        self.frame = 0
+        self.last_move_index = 6
+        self.attack = False
+        self.alive = True
+        self.dead = False
+        self.armor_broken = False
+
+        self.sword = 0
+        self.level = 1
+        self.armor = armor
+        self.money = 500
+
+        self.movement_dict = {
+            (pygame.K_LEFT,  False):   (0,  (-self.speed,  0)),
+            (pygame.K_RIGHT, False):   (1,  (self.speed,   0)),
+            (pygame.K_UP,    False):   (2,  (0,  -self.speed)),
+            (pygame.K_DOWN,  False):   (3,  (0,   self.speed)),
+            ('STAY',         False):   (4,  (0,            0)),
+            (pygame.K_LEFT,  True):    (5,  (-self.speed,  0)),
+            (pygame.K_RIGHT, True):    (6,  (self.speed,   0)),
+            (pygame.K_UP,    True):    (-1, (0,  -self.speed)),
+            (pygame.K_DOWN,  True):    (-1, (0,   self.speed)),
+            ('STAY',         True):    (-1, (0,            0))
+        }
+
+    def change_size(self, scale=1):
+        if not self.frame % 9:
+            image = self.armor[self.sword][4][self.frame % 4]
+            self.image = pygame.transform.scale(image, (image.get_width() * scale, image.get_height() * scale))
+
+        self.frame = (self.frame + 1) % 36
+
+    def update(self, pressed_button, attack, border):
+        action_index, player_movement = self.movement_dict.get((pressed_button, attack), self.movement_dict[('STAY', False)])
+        position, self.rect = self.rect.center, self.rect.move(player_movement)
+        self.image, image, step = PLAYER_RECT_IMAGE, self.image, 7
+
+        if self.hit_points <= 0 and self.alive:
+            self.frame = 0
+            self.alive = False
+
+        if attack and self.alive:
+            self.frame = 0
+            self.attack = True
+
+        if action_index in {0, 1, 5, 6}:
+            self.last_move_index = [5, 6][(action_index // 6 + action_index % 3) % 2]
+
+        if not self.alive:
+            self.make_a_murder()
+            self.rect.center = position
+            step, action_index = 30, 7
+
+        if self.attack:
+            self.make_an_attack()
+            action_index = self.last_move_index
+
+        if pygame.sprite.collide_mask(self, border):
+            self.rect.center = position
+            action_index = 4
+
+        self.image = self.change_image(action_index, image, step)
+
+    def make_a_murder(self):
+        if self.frame == 209:
+            self.dead = True
+
+    def make_an_attack(self):
+        opponent = pygame.sprite.spritecollideany(self, ENEMY_SPRITES)
+        if opponent:
+            opponent.hit_points -= self.damage
+        if self.frame == 42:
+            self.attack = False
+
+    def change_image(self, index, now_image, step):
+        self.frame += 1
+        if not self.frame % step:
+            return self.armor[self.sword][index][self.frame // step % len(self.armor[self.sword][index])]
+        return now_image
---
+++ vars.py
@@ -0,0 +1,69 @@
+import pygame
+
+from coin import Coin
+from player import Player
+from border import Border
+from button import Button, Item
+from images import                                                                                                                  \
+    START_GAME_BTN_IMAGE,     SHOP_BTN_IMAGE,         ARENA_BTN_IMAGE,         WINDOW,                                              \
+    CONTINUE_GAME_BTN_IMAGE,  LEAVE_GAME_BTN_IMAGE,   LEAVE_ARENA_BTN_IMAGE,   SETTINGS_BTN_IMAGE,        BACK_BTN_IMAGE,           \
+    SILVER_ARMOR_BTN_IMAGES,  GOLD_ARMOR_BTN_IMAGES,  RUBIN_ARMOR_BTN_IMAGES,  EMERALD_ARMOR_BTN_IMAGES,  DIAMOND_ARMOR_BTN_IMAGES, \
+    SILVER_SWORD_BTN_IMAGES,  GOLD_SWORD_BTN_IMAGES,  RUBIN_SWORD_BTN_IMAGES,  EMERALD_SWORD_BTN_IMAGES,  DIAMOND_SWORD_BTN_IMAGES, \
+    HEAL_BUFF_BTN_IMAGES,     ARMOR_BUFF_BTN_IMAGES,  SPEED_BUFF_BTN_IMAGES,   DAMAGE_BUFF_BTN_IMAGES,                              \
+    BORDER_IMAGE,             COIN_IMAGES,            SILVER_ARMOR,            PLAYER_RECT_IMAGE
+
+
+FPS = 60
+ARENA_LEVEL = 1
+CLOCK = pygame.time.Clock()
+MAKE_NEW_ENEMY_EVENT = pygame.USEREVENT + 1
+
+FONT = pygame.font.Font('assets/fonts/Comic Sans MS Pixel.ttf',       80)
+INFO_FONT = pygame.font.Font('assets/fonts/Comic Sans MS Pixel.ttf',  50)
+PRICE_FONT = pygame.font.Font('assets/fonts/Comic Sans MS Pixel.ttf', 40)
+
+
+border = Border(BORDER_IMAGE,                              (810,  375))
+player = Player(SILVER_ARMOR,                              (0,    290))
+coin = Coin(COIN_IMAGES,                                   (335,  25),   player.money, 80)
+
+start_btn = Button(START_GAME_BTN_IMAGE,             1,    WINDOW.get_rect().center)
+shop_btn = Button(SHOP_BTN_IMAGE,                   -1,    (170,  50),   scale=0.50)
+arena_btn = Button(ARENA_BTN_IMAGE,                 -2,    (1380, 895),  scale=0.65)
+back_btn = Button(BACK_BTN_IMAGE,                    1,    (237,  870),  scale=0.60)
+continue_game_btn = Button(CONTINUE_GAME_BTN_IMAGE,  1,    (805,  425),  scale=0.70)
+leave_game_btn = Button(LEAVE_GAME_BTN_IMAGE,        1,    (580,  590),  scale=0.40)
+leave_arena_btn = Button(LEAVE_ARENA_BTN_IMAGE,      1,    (1040, 590),  scale=0.40)
+settings_btn = Button(SETTINGS_BTN_IMAGE,            1,    (810,  515),  scale=0.40)
+
+silver_armor_btn = Item(SILVER_ARMOR_BTN_IMAGES,     0,    (750,  500),  scale=1.30)
+gold_armor_btn = Item(GOLD_ARMOR_BTN_IMAGES,         15,   (925,  500),  scale=1.30)
+rubin_armor_btn = Item(RUBIN_ARMOR_BTN_IMAGES,       35,   (1100, 500),  scale=1.30)
+emerald_armor_btn = Item(EMERALD_ARMOR_BTN_IMAGES,   50,   (1275, 500),  scale=1.30)
+diamond_armor_btn = Item(DIAMOND_ARMOR_BTN_IMAGES,   100,  (1450, 500),  scale=1.30)
+
+silver_sword_btn = Item(SILVER_SWORD_BTN_IMAGES,     0,    (750,  300),  scale=1.30)
+gold_sword_btn = Item(GOLD_SWORD_BTN_IMAGES,         15,   (925,  300),  scale=1.30)
+rubin_sword_btn = Item(RUBIN_SWORD_BTN_IMAGES,       35,   (1100, 300),  scale=1.30)
+emerald_sword_btn = Item(EMERALD_SWORD_BTN_IMAGES,   50,   (1275, 300),  scale=1.30)
+diamond_sword_btn = Item(DIAMOND_SWORD_BTN_IMAGES,   100,  (1450, 300),  scale=1.30)
+
+armor_buff_btn = Item(ARMOR_BUFF_BTN_IMAGES,         0,    (800,  700),  scale=1.30)
+heal_buff_btn = Item(HEAL_BUFF_BTN_IMAGES,           0,    (1000, 700),  scale=1.30)
+speed_buff_btn = Item(SPEED_BUFF_BTN_IMAGES,         0,    (1200, 700),  scale=1.30)
+damage_buff_btn = Item(DAMAGE_BUFF_BTN_IMAGES,       0,    (1400, 700),  scale=1.30)
+
+
+sword_text = FONT.render('ОРУЖИЕ',            True,  (255, 255, 255))
+armor_text = FONT.render('СНАРЯЖЕНИЕ',        True,  (255, 255, 255))
+buff_text = FONT.render('УСИЛИТЕЛИ',          True,  (255, 255, 255))
+shop_text = FONT.render('МАГАЗИН ПРЕДМЕТОВ',  True,  (255, 255, 255))
+
+pressed_buttons = ['STAY']
+window_top = 0
+
+
+pygame.draw.ellipse(BORDER_IMAGE,    pygame.Color(0, 255, 0),  (0, 0, 1450, 650), 5)
+pygame.draw.rect(PLAYER_RECT_IMAGE,  pygame.Color(0, 255, 0),  (0, 0, 150, 111))
+
+pygame.time.set_timer(MAKE_NEW_ENEMY_EVENT, 4000)
